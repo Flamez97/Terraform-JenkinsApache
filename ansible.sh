f5fd1bf9cb1fa450349fb74357bb2b34e3cdf9b9
O Ohio <tekzo4real@gmail.com>
2021-01-25 17:58:00 -0500

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -2,7 +2,6 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install ansible -y
-#curl http://checkip.amazonaws.com >>
 ansible -m ping localhost
 wget https://jenkinsci.s3.amazonaws.com/jenkins.yml
 ansible-playbook jenkins.yml
